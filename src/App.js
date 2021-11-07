60fcbc94b6aeaba69fec8408a8a59c6dcc5834ef
etozhesady <sadyrovanvar@gmail.com>
2021-11-08 00:39:19 +0600

    add cart open func

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import Drawer from './components/Drawer';
 
 function App() {
+  const [cartOpened, setCartOpened] = React.useState(false)
   return (
     <div className="wrapper clear">
-      <Drawer />
-
-      <Header />
+      {cartOpened ? <Drawer  onClose={() => setCartOpened(false)} /> : null}
+      <Header onClickCart={() => setCartOpened(true)}/>
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
